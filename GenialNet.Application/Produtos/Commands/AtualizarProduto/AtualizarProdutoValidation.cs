455d708635bee00cedbfdffa15765bf993f4ac19
RodrigoDelboni <rod.barbosa@gmail.com>
2024-08-26 22:35:19 -0300

    Removendo arquivos desnecessários e adicionando .gitignore

---
+++ GenialNet.Application/Produtos/Commands/AtualizarProduto/AtualizarProdutoValidation.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using GenialNet.Application.Produtos.Commands.CadastrarProduto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenialNet.Application.Produtos.Commands.AtualizarProduto
+{
+    internal class AtualizarProdutoValidation : AbstractValidator<AtualizarProdutoRequest>
+    {
+    }
+}
